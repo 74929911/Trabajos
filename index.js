06f9d2553648b01e547f0b12bdd4e41c23ea234e
Sergio Stanly <81574085+74929911@users.noreply.github.com>
2021-06-07 16:27:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+let numero4=prompt("Ingresa el primero numero")
+let numero5=prompt("Ingresa el segundo numero")
+
+numero4=parseInt(numero4)
+numero5=parseInt(numero5)
+
+// - definir una varaible tipo texto
+// - condicional si la var tipo texto tiene mas de 20 caracteres debe mostrar mensaje "cant caracteres" 
+
+
+
+
+// let preguntaEntrada= prompt("¿Cual es tu nombre?")
+// let preguntaRespuesta= alert(`Hola ${preguntaEntrada}`)
+// let preguntaEntrada1=prompt("¿Cuales son tus apellidos?")
+// let preguntaEdad=prompt("¿Cuantos años tienes?")
+// if(preguntaEdad >=18)
+// {
+// let numeroPasos = alert("A continuacion se te mostrara un pequeño tutotial de lo que contiene StarFlamer ")
+// let confirmarDatos=alert("Confirmanos tus datos")
+// let nombre = `Tu nombre es ${preguntaEntrada}
+// Tus apellidos son ${preguntaEntrada1} 
+// Tu edad es ${preguntaEdad}`
+// let respuesta=alert(nombre)
+// alert("Presiona el button para comenzar")
+// let frase =document.write("StarFlamer es una página hecha por fans para fans")
+// let frase1 =document.write("El objetivo es brindar información acerca de los videojuegos")}
+// else{
+// alert("Lo sentimos, no cuentas con el rango de edad establecido para ingresar")
+// }
+
